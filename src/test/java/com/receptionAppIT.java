f683f2887f446ba880dc72ea384e0f09f8d5b8f2
JoseJunior <joseademarti@gmail.com>
2022-05-28 19:07:49 -0300

    First commit project creation

---
+++ src/test/java/com/receptionAppIT.java
@@ -0,0 +1,9 @@
+package com;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class receptionAppIT extends receptionAppTest {
+
+    // Execute the same tests but in native mode.
+}
